289530ee7fc487a3ca72563c0d72c05df60bb7ce
Caleb Depatie <caleb.depatie@outlook.com>
2023-12-17 16:29:53 -0500

    initial kit of tests

--- test/test_helper.sh
+++ test/test_helper.sh
@@ -18,16 +18,20 @@ for file in $files; do
 
     test_expected=$(cat "$dir/$filename.out")
 
+    start_time=$(date +%s%N)  
+
     test_actual=$($interp $file | tail -n 1)
 
+    end_time=$(date +%s%N)  
+    execution_time=$(( (end_time - start_time) / 1000000 )) 
+
+
     if [ "$test_expected" != "$test_actual" ]; then
-        echo "[ FAIL ] $file"
-        echo "[ FAIL ] $file expected:"
-        echo $test_expected
-        echo "[ FAIL ] $file actual:"
-        echo $test_actual
+        echo "[ FAIL ] $file took $execution_time ms"
+        echo "[ FAIL ] $file expected: $test_expected"
+        echo "[ FAIL ] $file actual: $test_actual"
     else
-        echo "[ OK   ] $file"
+        echo "[ OK   ] $file took $execution_time ms"
     fi
 done
 
